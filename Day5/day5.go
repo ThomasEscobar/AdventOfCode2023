896bfc7e9f50e14ce81c42dfcc503ec9aaa3298a
ThomasEscobar <tescobar@vqcomms.com>
2022-12-14 00:41:22 +0000

    Lost in pointers...

--- Day5/day5.go
+++ Day5/day5.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	lines, err := readLines("input.txt")
+	lines, err := readLines("example.txt")
 	if err != nil {
 		log.Fatalf("Failed to read input file: %s", err)
 	}
@@ -24,23 +24,11 @@ func main() {
 }
 
 func solvePart1(lines []string) int {
-	stacksOfCratesLines := make([]string, 0)
-	instructions := make([]string, 0)
-	stacksOfCrates := make(map[int]Stack)
+	stacksOfCratesLines, instructions := parseInput(lines)
+	stacksOfCrates := identifyStacks(stacksOfCratesLines)
+	executeInstructions(&stacksOfCrates, instructions)
 
-	for _, line := range lines {
-		if line != "" && !strings.Contains(line, "move") {
-			stacksOfCratesLines = append(stacksOfCratesLines, line)
-		} else {
-			instructions = append(instructions, line)
-		}
-	}
-
-	for i, char := range stacksOfCratesLines[len(stacksOfCratesLines)-1] {
-		if char != ' ' {
-			var 
-		}
-	}
+	fmt.Printf("%v", stacksOfCrates)
 
 	return 0
 }
@@ -49,7 +37,71 @@ func solvePart2(lines []string) int {
 	return 0
 }
 
-// To implement: https://www.educative.io/answers/how-to-implement-a-stack-in-golang
+func parseInput(lines []string) ([]string, []string) {
+	stacksOfCratesLines := make([]string, 0)
+	instructions := make([]string, 0)
+
+	for _, line := range lines {
+		if line != "" && !strings.Contains(line, "move") {
+			stacksOfCratesLines = append(stacksOfCratesLines, line)
+		} else if line != "" {
+			instructions = append(instructions, line)
+		}
+	}
+
+	return stacksOfCratesLines, instructions
+}
+
+func identifyStacks(stacksOfCratesLines []string) map[int]Stack {
+	stacksOfCrates := make(map[int]Stack)
+
+	for x, stackIndex := range stacksOfCratesLines[len(stacksOfCratesLines)-1] {
+		if stackIndex != ' ' {
+			tmpStack := make(Stack, 0)
+			// -1 because array starts at 0 and -1 because we don't want the last line
+			for y := len(stacksOfCratesLines) - 2; y >= 0; y-- {
+				var stackChar = string(stacksOfCratesLines[y][x])
+				if stackChar != " " {
+					tmpStack = append(tmpStack, stackChar)
+				}
+			}
+			stacksOfCrates[convertRuneToDigit(stackIndex)] = tmpStack
+		}
+	}
+
+	return stacksOfCrates
+}
+
+func executeInstructions(p *map[int]Stack, instructions []string) {
+	stackOfCrates := *p
+
+	for _, instruction := range instructions {
+		fmt.Printf("Before executing instruction '%v': %v\n", instruction, stackOfCrates)
+
+		moveCount := convertRuneToDigit(rune(instruction[5]))
+		fromIndex := convertRuneToDigit(rune(instruction[12]))
+		toIndex := convertRuneToDigit(rune(instruction[17]))
+
+		for i := 0; i < moveCount; i++ {
+			fromStack := stackOfCrates[fromIndex]
+			toStack := stackOfCrates[toIndex]
+			movingCrate, _ := fromStack.Pop()
+			toStack.Push(movingCrate)
+
+			fmt.Printf("Moved %v from %v to %v\n", movingCrate, fromIndex, toIndex)
+		}
+
+		fmt.Printf("After executing: %v\n", stackOfCrates)
+	}
+}
+
+func convertRuneToDigit(r rune) int {
+	digit := int(r - '0')
+	if digit >= 0 && digit <= 10 {
+		return digit
+	}
+	return -1
+}
 
 // readLines reads a whole file into memory
 // and returns a slice of its lines.
