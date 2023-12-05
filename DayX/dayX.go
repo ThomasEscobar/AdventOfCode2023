9a658af4dbe86f00841dfb79353882fe6e5b41f9
ThomasEscobar <thomas.escobarmunoz@gmail.com>
2023-12-05 23:17:08 +0000

    Not sure where i'm going

---
+++ DayX/dayX.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	lines, err := readLines("input.txt")
+	if err != nil {
+		log.Fatalf("Failed to read input file: %s", err)
+	}
+
+	// Part 1
+	solutionPart1 := solvePart1(lines)
+	fmt.Printf("Solution for part 1 is %d\n", solutionPart1)
+
+	// Part 2
+	solutionPart2 := solvePart2(lines)
+	fmt.Printf("Solution for part 2 is %d\n", solutionPart2)
+}
+
+func solvePart1(lines []string) int {
+	return -1
+}
+
+func solvePart2(lines []string) int {
+
+	return -1
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
